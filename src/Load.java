4ae6e250db1144636944bc83bd97a739666a4cbb
ksethi6 <79504900+ksethi6@users.noreply.github.com>
2021-03-16 04:56:21 +0530

    Add files via upload

--- src/Load.java
+++ src/Load.java
@@ -1,50 +1,61 @@
-import java.io.*;
-import java.util.Scanner;
-import javax.swing.JFileChooser;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
+/**
+ *
+ * @author Keshav
+ * @since 03-16-2020
+ *
+ *
+ */
+class Load implements ActionListener {
+    private String fileName;
+    //private JTabbedPane pane;
+    //private Frame frame;
 
-public class Load {
-    /*************************************************************************************
-     *  - Method Name: load()
-     *  - Input Parameters : WorkingPanel workingPanel
-     *  - Return Type :void
-     *  - Author : Keshav
-     *  - Creation Date : 03/08/2021
-     *  - Desc: Loads the content of file into the Working panel object.
-     ***************************************************************************************/
-    public static void load(WorkingPanel workingPanel) {
-        File file;
-        Scanner fileIn;
-        int response;
-        JFileChooser chooser = new JFileChooser("");
-
-        chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
-        response = chooser.showOpenDialog(null);
-
-        if(response == JFileChooser.APPROVE_OPTION){
-            file = chooser.getSelectedFile();
-
-            try {
-                fileIn = new Scanner(file);
-                if(file.isFile()){
-                    while(fileIn.hasNextLine()){
-                        String line = fileIn.nextLine();
-                        System.out.println(line);
-                        /**In Progress**/
-                    }
-
-                }
-                else{
-                    System.out.println("Incorrect file format!");
-                }
-                fileIn.close();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            }
-        }
-
+    public void Load() {
+      //  this.frame = frame;
     }
 
+    public void actionPerformed(ActionEvent e) {
+        FileInputStream fileInStream = null;
+        ObjectInputStream objectInStream = null;
+        Component[] tabsToOpen;
+        try {
+            JFileChooser chosenFile = new JFileChooser();
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("SER516", "ser");
+            chosenFile.setFileFilter(filter);
+            int showOpenDialog = chosenFile.showOpenDialog(null);
+            if (showOpenDialog == JFileChooser.APPROVE_OPTION) {
+                fileName = chosenFile.getSelectedFile().getAbsolutePath();
+                fileInStream = new FileInputStream(fileName);
+                objectInStream = new ObjectInputStream(fileInStream);
+                tabsToOpen = (Component[]) objectInStream.readObject();
+                JFrame frame = new JFrame();
+                Container content = frame.getContentPane();
+                content.removeAll();
+                content.add(new AppMain("new_file"), BorderLayout.NORTH);
+                JTabbedPane pane = new JTabbedPane();
+                content.add(pane, BorderLayout.CENTER);
+                content.add(new LeftPanel(), BorderLayout.WEST);
+
+            }
+            if (objectInStream != null) {
+                objectInStream.close();
+            }
+            if (fileInStream != null) {
+                fileInStream.close();
+            }
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (ClassNotFoundException e1) {
+        }
+    }
 }
-
-
-
